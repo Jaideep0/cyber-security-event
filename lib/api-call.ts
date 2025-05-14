5d0435ec86c427b83ad2d8b0095875556cfcf86e
Jaideep Singh <89267664+Jaideep0@users.noreply.github.com>
2025-05-14 15:03:08 -0400

    Update api-call.ts

--- lib/api-call.ts
+++ lib/api-call.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiCall = axios.create({
-  baseURL: `${'http://localhost:8000'}/api`,
+  baseURL: `${process.env.BACKEND_BASE_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
